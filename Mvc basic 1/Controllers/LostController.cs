4ae0003b4ac8e83ec33373cd49fbb453598ca5cb
jacob jolsö <jacke_j_1998@hotmail.se>
2020-01-08 16:07:27 +0100

    bara layout kvar

---
+++ Mvc basic 1/Controllers/LostController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc_basic_1.Controllers
+{
+    public class LostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
