89ba477162dd11e90334ad23dc5f07c496a0e9d2
jacob jolsö <jacke_j_1998@hotmail.se>
2020-01-10 15:53:56 +0100

    kan se menyn länkarna

--- Mvc basic 1/Startup.cs
+++ Mvc basic 1/Startup.cs
@@ -32,10 +32,10 @@ namespace Mvc_basic_1
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
